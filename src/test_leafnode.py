24f4f1d108a2075e315c90cab8de9099e9e57f14
ethpalser <eth.palser@gmail.com>
2024-03-06 14:01:18 -0500

    added leafnode.py and tests

---
+++ src/test_leafnode.py
@@ -0,0 +1,16 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_to_html(self):
+        leafnodeA = LeafNode("p", "Hello World")
+        expectedA = "<p>Hello World</p>"
+        actualA = leafnodeA.to_html()
+        self.assertEqual(expectedA, actualA)
+
+        leafnodeB = LeafNode("a", "google.com", {"href": "https://www.google.com"})
+        expectedB = "<a href=\"https://www.google.com\">google.com</a>"
+        actualB = leafnodeB.to_html()
+        self.assertEqual(expectedB, actualB)
