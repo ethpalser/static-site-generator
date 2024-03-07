863f55738eafe6c93163518bbd89321983119943
ethpalser <eth.palser@gmail.com>
2024-03-07 14:32:30 -0500

    converted blocks to html node

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,8 @@
 import re
 from parentnode import ParentNode
+from leafnode import LeafNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
 
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
@@ -40,3 +43,70 @@ def block_to_block_type(block):
             num += 1
         return block_type_ordered_list
     return block_type_paragraph
+
+# block_to_html_node
+
+def block_to_heading_html_node(block):
+    if block_to_block_type(block) != block_type_heading:
+        raise ValueError("Cannot convert non-heading block into a heading node")
+    heading_count = 0
+    for i in range(0, len(block)):
+        if block[i] != "#":
+            break
+        heading_count += 1
+    return ParentNode(f"h{heading_count}", None, list(map(text_node_to_html_node, text_to_textnodes(block[heading_count + 1:]))))
+    
+def block_to_code_html_node(block):
+    if block_to_block_type(block) != block_type_code:
+        raise ValueError("Cannot convert non-code block into a code node")
+    return ParentNode("pre", None, [LeafNode("code", block[3:-3])])
+
+def block_to_quote_html_node(block):
+    if block_to_block_type(block) != block_type_quote:
+        raise ValueError("Cannot convert non-quote block into a quote node")
+    value = "\n".join(map(lambda str : str[2:], block.split("\n")))
+    return ParentNode("blockquote", None, list(map(text_node_to_html_node, text_to_textnodes(value))))
+
+def block_to_unordered_list_html_node(block):
+    if block_to_block_type(block) != block_type_unordered_list:
+        raise ValueError("Cannot convert non-unordered-list block into a unordered list node")
+    list_nodes = []
+    items = map(lambda str : str[2:], block.split("\n"))
+    for item in items:
+        list_nodes.append(ParentNode("li", None, list(map(text_node_to_html_node, text_to_textnodes(item)))))
+    return ParentNode("ul", None, list_nodes)
+
+def block_to_ordered_list_html_node(block):
+    if block_to_block_type(block) != block_type_ordered_list:
+        raise ValueError("Cannot convert non-ordered-list block into a ordered list node")
+    list_nodes = []
+    items = block.split("\n")
+    for i in range(0, len(items)):
+        list_nodes.append(ParentNode("li", None, list(map(text_node_to_html_node, text_to_textnodes(items[i][3 + i//10:])))))
+    return ParentNode("ol", None, list_nodes)
+
+def block_to_paragraph_html_node(block):
+    if block_to_block_type(block) != block_type_paragraph:
+        raise ValueError("Cannot convert non-paragraph block into a paragraph node")
+    return ParentNode("p", None, list(map(text_node_to_html_node, text_to_textnodes(block))))
+
+# markdown_to_html_node
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == block_type_heading:
+            nodes.append(block_to_heading_html_node(block))
+        elif block_type == block_type_code:
+            nodes.append(block_to_code_html_node(block))
+        elif block_type == block_type_quote:
+            nodes.append(block_to_quote_html_node(block))
+        elif block_type == block_type_unordered_list:
+            nodes.append(block_to_unordered_list_html_node(block))
+        elif block_type == block_type_ordered_list:
+            nodes.append(block_to_ordered_list_html_node(block))
+        else:
+            nodes.append(block_to_paragraph_html_node(block))
+    return ParentNode("div", None, nodes)
