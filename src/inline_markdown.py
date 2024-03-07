291663b5f90ddb288dd86843f63979d7e7ab6772
ethpalser <eth.palser@gmail.com>
2024-03-06 23:48:28 -0500

    added split node for images and links with a test each

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -33,10 +33,61 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 def extract_markdown_images(text):
     if text == None:
-        return None
+        return []
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
 def extract_markdown_links(text):
     if text == None:
-        return None
+        return []
     return re.findall(r"[^!]\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    if old_nodes == None:
+        return None
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode) or node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        images = extract_markdown_images(node.text)
+        if len(images) == 0:
+            new_nodes.append(node)
+        else:
+            node_split = re.split(r"!\[(.*?)\]\((.*?)\)", node.text)
+            for match in images:
+                node_split.remove(match[0])
+                node_split.remove(match[1])
+            img_pos = 0
+            for i in range(0, len(node_split)):
+                if node_split[i] != "":
+                    new_nodes.append(TextNode(node_split[i], text_type_text))
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(images[img_pos][0], text_type_image, images[img_pos][1]))
+                    img_pos += 1
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    if old_nodes == None:
+        return None
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode) or node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        links = extract_markdown_links(node.text)
+        if len(links) == 0:
+            new_nodes.append(node)
+        else:
+            node_split = re.split(r"(?:(?<!!))\[(.*?)\]\((.*?)\)", node.text)
+            for match in links:
+                node_split.remove(match[0])
+                node_split.remove(match[1])
+            link_pos = 0
+            for i in range(0, len(node_split)):
+                if node_split[i] != "":
+                    new_nodes.append(TextNode(node_split[i], text_type_text))
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(links[link_pos][0], text_type_link, links[link_pos][1]))
+                    link_pos += 1
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -2,14 +2,18 @@ import unittest
 from inline_markdown import (
     split_nodes_delimiter,
     extract_markdown_images,
-    extract_markdown_links
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link
 )
 from textnode import (
     TextNode,
     text_type_text,
     text_type_bold,
     text_type_italic,
-    text_type_code
+    text_type_code,
+    text_type_image,
+    text_type_link
 )
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -64,4 +68,26 @@ class TestInlineMarkdown(unittest.TestCase):
         text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
         expected = [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
         actual = extract_markdown_links(text)
+        self.assertEqual(expected, actual)    
+
+    def test_split_nodes_image_given_image(self):
+        node = [TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![another](https://i.imgur.com/dfsdkjfd.png)", text_type_text)]
+        expected = [
+            TextNode("This is text with an ", text_type_text),
+            TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", text_type_text),
+            TextNode("another", text_type_image, "https://i.imgur.com/dfsdkjfd.png")
+        ]
+        actual = split_nodes_image(node)
+        self.assertEqual(expected, actual)
+
+    def test_split_nodes_link_given_link(self):
+        node = [TextNode("This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)", text_type_text)]
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.example.com"),
+            TextNode(" and ", text_type_text),
+            TextNode("another", text_type_link, "https://www.example.com/another")
+        ]
+        actual = split_nodes_link(node)
         self.assertEqual(expected, actual)
