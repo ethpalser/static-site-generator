fded0fbbbf258141a3ebd87e372a4dbbf02ce5e3
ethpalser <eth.palser@gmail.com>
2024-03-07 23:52:13 -0500

    implemented copying directory from static into public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
+import os
+import shutil
 from textnode import TextNode
 
 def main():
-    node = TextNode("This is text with a `code block` word", "text")
-    print(node)
+    
+    copy_from_directory("./static", "./public")
+
+def copy_from_directory(root_dir, target_root_dir, is_root = True):
+    if not os.path.exists(root_dir):
+        raise Exception("The directory to copy does not exist")
+    if is_root:
+        shutil.rmtree(target_root_dir)
+        os.mkdir(target_root_dir)
+    for child in os.listdir(root_dir):
+        src_path = os.path.join(root_dir, child)
+        dst_path = os.path.join(target_root_dir, child)
+        print(f"{src_path} ---> {dst_path}")
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+        else:
+            if not os.path.exists(dst_path):
+                os.mkdir(dst_path)
+            copy_from_directory(src_path, dst_path, False)
+
 
 main()
